e26bfb8e22d7d7816a70e99578f394ad8706a49a
jpiskozub <jpiskozub@gmail.com>
2021-11-16 23:51:43 +0100

    Create zmq.py

---
+++ zmq.py
@@ -0,0 +1,18 @@
+
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://192.168.43.137:5555")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(10):
+    print(f"Sending request {request} …")
+    socket.send(b"Hello")
+
+    #  Get the reply.
+    message = socket.recv()
+    print(f"Received reply {request} [ {message} ]")
